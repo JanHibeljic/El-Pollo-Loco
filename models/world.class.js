77492c88b6f5b1d0d7804029fa02a911e7865f5e
Jan Hibeljic <j.hibeljic@gmx.de>
2025-01-01 20:56:16 +0100

    added reclear canvas and draw enemies

--- models/world.class.js
+++ models/world.class.js
@@ -1,5 +1,38 @@
 class World {
   character = new Character();
   enemies = [new Chicken(), new Chicken(), new Chicken()];
-  draw() {}
+  canvas; //benötigt zum reclear
+  ctx;
+  constructor(canvas) {
+    this.ctx = canvas.getContext("2d");
+    this.canvas = canvas; //benötigt zum reclear
+    this.draw();
+  }
+  draw() {
+    //reclear canvas
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    this.ctx.drawImage(
+      this.character.img,
+      this.character.x,
+      this.character.y,
+      this.character.width,
+      this.character.height
+    );
+    //jeder enemy wird einzeln gezeichnet also 3xchicken
+    this.enemies.forEach((enemy) => {
+      this.ctx.drawImage(
+        enemy.img,
+        enemy.x,
+        enemy.y,
+        enemy.width,
+        enemy.height
+      );
+    });
+    //draw() wird immer wieder aufgerufen
+    let self = this;
+    requestAnimationFrame(function () {
+      self.draw();
+    });
+  }
 }
