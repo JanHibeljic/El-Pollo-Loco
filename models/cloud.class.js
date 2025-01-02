4abdc80f4d2c4c17519c310b99a88335437434a0
Jan Hibeljic <j.hibeljic@gmx.de>
2025-01-02 14:01:16 +0100

    animate clouds added

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -5,5 +5,12 @@ class Cloud extends MoveableObject {
   constructor() {
     super().loadImage("img_pollo_locco/img/5_background/layers/4_clouds/1.png");
     this.x = Math.random() * 500;
+    this.animate();
+  }
+
+  animate() {
+    setInterval(() => {
+      this.x -= 0.15;
+    }, 1000 / 60);
   }
 }
