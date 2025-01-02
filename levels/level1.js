f9a4c1241311557d76cafa3ed4c26e64e23ca188
Jan Hibeljic <j.hibeljic@gmx.de>
2025-01-02 23:09:03 +0100

    added new folder  levels ->level1 for added elements for world

---
+++ levels/level1.js
@@ -0,0 +1,87 @@
+const level1 = new Level(
+  [new Chicken(), new Chicken(), new Chicken()],
+  [new Cloud()],
+  [
+    new BackgroundObject(
+      "img_pollo_locco/img/5_background/layers/air.png",
+      -719
+    ),
+    new BackgroundObject(
+      "img_pollo_locco/img/5_background/layers/3_third_layer/2.png",
+      -719
+    ), // y wird nicht mehr übergeben für die höhe sondern im jeweiligen Objekt berechnet und übergeben
+    new BackgroundObject(
+      "img_pollo_locco/img/5_background/layers/2_second_layer/2.png",
+      -719
+    ),
+    new BackgroundObject(
+      "img_pollo_locco/img/5_background/layers/1_first_layer/2.png",
+      -719
+    ),
+
+    new BackgroundObject("img_pollo_locco/img/5_background/layers/air.png", 0),
+    new BackgroundObject(
+      "img_pollo_locco/img/5_background/layers/3_third_layer/1.png",
+      0
+    ), // y wird nicht mehr übergeben für die höhe sondern im jeweiligen Objekt berechnet und übergeben
+    new BackgroundObject(
+      "img_pollo_locco/img/5_background/layers/2_second_layer/1.png",
+      0
+    ),
+    new BackgroundObject(
+      "img_pollo_locco/img/5_background/layers/1_first_layer/1.png",
+      0
+    ),
+
+    new BackgroundObject(
+      "img_pollo_locco/img/5_background/layers/air.png",
+      719
+    ),
+    new BackgroundObject(
+      "img_pollo_locco/img/5_background/layers/3_third_layer/2.png",
+      719
+    ), // y wird nicht mehr übergeben für die höhe sondern im jeweiligen Objekt berechnet und übergeben
+    new BackgroundObject(
+      "img_pollo_locco/img/5_background/layers/2_second_layer/2.png",
+      719
+    ),
+    new BackgroundObject(
+      "img_pollo_locco/img/5_background/layers/1_first_layer/2.png",
+      719
+    ),
+
+    new BackgroundObject(
+      "img_pollo_locco/img/5_background/layers/air.png",
+      1438
+    ),
+    new BackgroundObject(
+      "img_pollo_locco/img/5_background/layers/3_third_layer/1.png",
+      1438
+    ), // y wird nicht mehr übergeben für die höhe sondern im jeweiligen Objekt berechnet und übergeben
+    new BackgroundObject(
+      "img_pollo_locco/img/5_background/layers/2_second_layer/1.png",
+      1438
+    ),
+    new BackgroundObject(
+      "img_pollo_locco/img/5_background/layers/1_first_layer/1.png",
+      1438
+    ),
+
+    new BackgroundObject(
+      "img_pollo_locco/img/5_background/layers/air.png",
+      2157
+    ),
+    new BackgroundObject(
+      "img_pollo_locco/img/5_background/layers/3_third_layer/2.png",
+      2157
+    ), // y wird nicht mehr übergeben für die höhe sondern im jeweiligen Objekt berechnet und übergeben
+    new BackgroundObject(
+      "img_pollo_locco/img/5_background/layers/2_second_layer/2.png",
+      2157
+    ),
+    new BackgroundObject(
+      "img_pollo_locco/img/5_background/layers/1_first_layer/2.png",
+      2157
+    ),
+  ]
+);
