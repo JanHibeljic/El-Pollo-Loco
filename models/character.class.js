3bf2612a977a6f64706a1bca1a3ab0e82d3517a0
Jan Hibeljic <j.hibeljic@gmx.de>
2025-01-02 21:29:52 +0100

    character is moving left and right on canvas

--- models/character.class.js
+++ models/character.class.js
@@ -1,6 +1,7 @@
 class Character extends MoveableObject {
   height = 260;
   y = 170;
+  speed = 6;
   IMAGES_WALKING = [
     "img_pollo_locco/img/2_character_pepe/2_walk/W-21.png",
     "img_pollo_locco/img/2_character_pepe/2_walk/W-22.png",
@@ -21,14 +22,26 @@ class Character extends MoveableObject {
 
   animate() {
     setInterval(() => {
+      //walk on canvas
       if (this.world.keyboard.RIGHT) {
+        this.x += this.speed;
+      }
+
+      if (this.world.keyboard.LEFT) {
+        this.x -= this.speed;
+      }
+    }, 1000 / 60);
+
+    setInterval(() => {
+      if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+        //walkanimation
         let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 7 % 6;     1, Rest 1
         // i = 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0
         let path = this.IMAGES_WALKING[i]; // anstatt [i] war vorher [this.currentImage]
         this.img = this.imageCache[path];
         this.currentImage++;
       }
-    }, 100); //ms
+    }, 50); //ms
   }
 
   jump() {}
