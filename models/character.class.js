5a6339d0980f336e0c6a4c99fa9a920a2aa9aaa3
Jan Hibeljic <j.hibeljic@gmx.de>
2025-01-03 21:43:57 +0100

    added jumpanimation to character

--- models/character.class.js
+++ models/character.class.js
@@ -1,6 +1,6 @@
 class Character extends MoveableObject {
   height = 260;
-  y = 170;
+  y = 100;
   speed = 6;
   IMAGES_WALKING = [
     "img_pollo_locco/img/2_character_pepe/2_walk/W-21.png",
@@ -10,6 +10,18 @@ class Character extends MoveableObject {
     "img_pollo_locco/img/2_character_pepe/2_walk/W-25.png",
     "img_pollo_locco/img/2_character_pepe/2_walk/W-26.png",
   ];
+
+  IMAGES_JUMPING = [
+    "img_pollo_locco/img/2_character_pepe/3_jump/J-31.png",
+    "img_pollo_locco/img/2_character_pepe/3_jump/J-32.png",
+    "img_pollo_locco/img/2_character_pepe/3_jump/J-33.png",
+    "img_pollo_locco/img/2_character_pepe/3_jump/J-34.png",
+    "img_pollo_locco/img/2_character_pepe/3_jump/J-35.png",
+    "img_pollo_locco/img/2_character_pepe/3_jump/J-36.png",
+    "img_pollo_locco/img/2_character_pepe/3_jump/J-37.png",
+    "img_pollo_locco/img/2_character_pepe/3_jump/J-38.png",
+    "img_pollo_locco/img/2_character_pepe/3_jump/J-39.png",
+  ];
   world;
   walking_sound = new Audio("audio/walking_on_sand.mp3");
 
@@ -17,6 +29,8 @@ class Character extends MoveableObject {
     super().loadImage("img_pollo_locco/img/2_character_pepe/2_walk/W-21.png");
 
     this.loadImages(this.IMAGES_WALKING);
+
+    this.loadImages(this.IMAGES_JUMPING);
 
     this.applyGravity();
 
@@ -42,15 +56,19 @@ class Character extends MoveableObject {
     }, 1000 / 60);
 
     setInterval(() => {
-      if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
-        //walkanimation
-        let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 7 % 6;     1, Rest 1
-        // i = 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0
-        let path = this.IMAGES_WALKING[i]; // anstatt [i] war vorher [this.currentImage]
-        this.img = this.imageCache[path];
-        this.currentImage++;
+      if (this.isAboveGround()) {
+        this.playAnimation(this.IMAGES_JUMPING); //jump or walk animation
+      } else {
+        if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+          //walkanimation
+          let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 7 % 6;     1, Rest 1
+          // i = 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0
+          let path = this.IMAGES_WALKING[i]; // anstatt [i] war vorher [this.currentImage]
+          this.img = this.imageCache[path];
+          this.currentImage++;
+        }
       }
-    }, 50); //ms
+    }, 1000 / 60); //ms
   }
 
   jump() {}
