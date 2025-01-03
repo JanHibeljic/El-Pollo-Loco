542254498814a83df9fd027266efdf44aa65e95c
Jan Hibeljic <j.hibeljic@gmx.de>
2025-01-03 10:32:50 +0100

    added various game sounds in audiofolder. walking sound added for character

--- models/character.class.js
+++ models/character.class.js
@@ -11,6 +11,7 @@ class Character extends MoveableObject {
     "img_pollo_locco/img/2_character_pepe/2_walk/W-26.png",
   ];
   world;
+  walking_sound = new Audio("audio/walking_on_sand.mp3");
 
   constructor() {
     super().loadImage("img_pollo_locco/img/2_character_pepe/2_walk/W-21.png");
@@ -22,15 +23,18 @@ class Character extends MoveableObject {
 
   animate() {
     setInterval(() => {
+      this.walking_sound.pause();
       //walk on canvas
       if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
         this.x += this.speed;
         this.otherDirection = false;
+        this.walking_sound.play();
       }
 
       if (this.world.keyboard.LEFT && this.x > 0) {
         this.x -= this.speed;
         this.otherDirection = true;
+        this.walking_sound.play();
       }
       this.world.camera_x = -this.x + 100;
     }, 1000 / 60);
